773f801f7d496d1438fe1a9d6fd83177a3c3dd68
Rikky <rikkster@yandex.ru>
2020-08-11 21:32:59 +0300

    Показывается в тот момент пока axios подтягивает данные

---
+++ components/loader.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { styles } from '../styles/global';
+
+export const Loader = () => <Text style={styles.loader}>LOADING</Text>;
